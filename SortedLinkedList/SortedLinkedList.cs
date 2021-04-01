c6e6e3bada480a9c4d4dac8f6465b8f0365bb9c2
SaltyScarf <mykyta.onyshchenko@nure.ua>
2021-04-02 01:26:08 +0300

    SLL fields and properties

--- SortedLinkedList/SortedLinkedList.cs
+++ SortedLinkedList/SortedLinkedList.cs
@@ -1,7 +1,17 @@
-﻿namespace SortedLinkedList
+﻿using System;
+
+namespace SortedLinkedList
 {
-    public class SortedLinkedList
+    public class SortedLinkedList<T> where T : IComparable<T>
     {
+        private int _count;
+        private Node<T> Head;
+        private Node<T> Tail;
         
+        public int Count { get{ return _count; }}
+        
+        public Node<T> _Head { get{ return Head; }}
+        
+        public Node<T> _Tail { get{ return Tail; }}
     }
 }
