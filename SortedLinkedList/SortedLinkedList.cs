ae1a7b4f0c82b454f5b4ad6956e4f12c25053d2e
SaltyScarf <mykyta.onyshchenko@nure.ua>
2021-04-02 01:32:53 +0300

    Class SLL method AddItem

--- SortedLinkedList/SortedLinkedList.cs
+++ SortedLinkedList/SortedLinkedList.cs
@@ -13,5 +13,55 @@ namespace SortedLinkedList
         public Node<T> _Head { get{ return Head; }}
         
         public Node<T> _Tail { get{ return Tail; }}
+
+        public void AddItem(T data)
+        {
+            Node<T> temp = new Node<T>(data);
+
+            if (Head == null)
+            {
+                Head = temp;
+                Tail = Head;
+            }
+            else
+            {
+                Node<T> currentNode = Head;
+                Node<T> previousNode = null;
+
+                while (true)
+                {
+                    if (data.CompareTo(currentNode.Data) == 1)
+                    {
+                        if (currentNode.NextNode == null)
+                        {
+                            currentNode.NextNode = temp;
+                            Tail = temp;
+                            
+                            break;
+                        }
+
+                        previousNode = currentNode;
+                        currentNode = currentNode.NextNode;
+                    }
+                    else
+                    {
+                        if (previousNode == null)
+                        {
+                            Head = temp;
+                            Head.NextNode = currentNode;
+                            
+                            break;
+                        }
+
+                        previousNode.NextNode = temp;
+                        temp.NextNode = currentNode;
+                        
+                        break;
+                    }
+                }
+            }
+        }
+        
+        
     }
 }
